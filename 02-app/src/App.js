472ccf6126d52382b066b78527511f6a50f6ca99
Nausheen Faiyaz <logme2nausheen@gmail.com>
2025-12-04 19:51:41 +0530

    Comment out unused imports in App.js

--- 02-app/src/App.js
+++ 02-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from "react";
+// import React, { useEffect,useState } from "react";
 import "./App.css";
 // import MyComponent from "./MyComp";
 import Timer from "./Timer";
