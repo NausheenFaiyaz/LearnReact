f2eb33758146eb20a7ee56fc2c46d50827d4819d
Nausheen Faiyaz <logme2nausheen@gmail.com>
2025-12-04 19:50:22 +0530

    Build tic-tac-toe game and understood how useState works

---
+++ tic-tac-toe/src/component/Square.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Square=(props)=>{
+    
+    return(
+        <div className="square" onClick={props.onClick}>
+            <h1>{props.value}</h1>
+        </div>
+    )
+}
+
+export default Square
